e151bf9a1f206fd5ebfac17d3e9c42b58a89e599
bbb1g <dlwnsdh3@gmail.com>
2017-09-23 06:49:12 -0700

    Update parser.c

--- packet_parser/parser.c
+++ packet_parser/parser.c
@@ -1,48 +1,28 @@
 #include<pcap.h>
 #include<stdio.h>
+#include<libnet.h>
 
-#define ETHER_ADDR_LEN 6
-
-struct sniff_ethernet {
-	u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
-	u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
-	u_short ether_type; /* IP? ARP? RARP? etc */
-};
-
-
+void handler(u_char * arg,const struct pcap_pkthdr * pkthdr,const u_char * packet)
+{
+	printf("I got packet!\n");
+}	
 int main(int argc,char * argv[],char * env[]){
 	pcap_t * handle;
 	char errbuf[PCAP_ERRBUF_SIZE]={};
 	char * dev;
-	struct bpf_program fp;
-	char filter_exp[] = "port 23";
-	bpf_u_int32 mask;
-	bpf_u_int32 net;
-	struct pcap_pkthdr header;
-	const u_char * packet;
+	int cnt;
 
 	dev = "ens33";
-
-	if(pcap_lookupnet(dev,&net,&mask,errbuf) == -1){
-		printf("pcap_lookupnet error");
-	}
 
 	handle = pcap_open_live(dev,BUFSIZ,1,1000,errbuf);
 	if(handle==NULL){
 		printf("pcap_open_live error\n%s\n",errbuf);
 		return(2);
 	}
-	if(pcap_compile(handle,&fp,filter_exp,0,net)==-1){
-		printf("pcap_compile error\n%s\n",pcap_geterr(handle));
-		return(2);
-	}
-	if(pcap_setfilter(handle,&fp)==-1){
-		printf("setfilter error\n");
-		return(2);
-	}
-	packet = pcap_next(handle,&header);
 
-	printf("Jacked a packet with length of [%d]\n",header.len);
-	pcap_close(handle);
+	if (pcap_loop(handle,-1,handler,(u_char *)&cnt) == -1){
+		printf("pcap_loop error!");
+		return(2);
+	}
 	return(0);
 }

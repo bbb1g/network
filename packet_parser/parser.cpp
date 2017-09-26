de9d79a0b9ba01cb5695324edfa75fa35d4b8d6b
bbb1g <30920085+bbb1g@users.noreply.github.com>
2017-09-26 22:13:01 +0900

    Update parser.cpp

--- packet_parser/parser.cpp
+++ packet_parser/parser.cpp
@@ -5,16 +5,36 @@
 void handler(u_char * arg,const struct pcap_pkthdr * pkthdr,const u_char * packet)
 {
 	libnet_ethernet_hdr * eth = (libnet_ethernet_hdr *)packet;
-	printf("Ethernet source mac      : %02x:%02x:%02x:%02x:%02x:%02x\n",
+	uint16_t eth_type = ((eth->ether_type & 0xff)<<8) + 
+		                ((eth->ether_type & 0xff00)>>8);
+
+	libnet_ipv4_hdr * ip = (libnet_ipv4_hdr *)(packet + sizeof(libnet_ethernet_hdr));
+	libnet_tcp_hdr * tcp = (libnet_tcp_hdr *)((u_char *)ip+sizeof(libnet_ipv4_hdr));
+
+	if(eth_type != 0x0800)return;
+
+	puts("-----------------------------------");
+	//ETHERNET HEADER PARSING
+	puts("+ETHERNET HEADER+");
+	printf("Ethernet src mac : %02x:%02x:%02x:%02x:%02x:%02x\n",
 			eth->ether_shost[0],eth->ether_shost[1],
 			eth->ether_shost[2],eth->ether_shost[3],
 			eth->ether_shost[4],eth->ether_shost[5]);
-	printf("Ethernet destination mac : %02x:%02x:%02x:%02x:%02x:%02x\n",
+	printf("Ethernet dst mac : %02x:%02x:%02x:%02x:%02x:%02x\n",
 			eth->ether_dhost[0],eth->ether_dhost[1],
 			eth->ether_dhost[2],eth->ether_dhost[3],
 			eth->ether_dhost[4],eth->ether_dhost[5]);
-	printf("Type : 0x%02x%02x\n",(eth->ether_type & 0xff),(eth->ether_type & 0xff00)>>8);
-}	
+	printf("Ethernet Type : 0x%04x\n",eth_type);
+	putchar(10);
+
+	//IP HEADER PARSING
+	puts("+IP HEADER+");
+	printf("IP's src ip : %s\n",inet_ntoa(ip->ip_src));
+	printf("IP's dst ip : %s\n",inet_ntoa(ip->ip_dst));
+	puts("-----------------------------------");
+	printf("\n\n");
+}
+
 int main(int argc,char * argv[],char * env[]){
 	pcap_t * handle;
 	char errbuf[PCAP_ERRBUF_SIZE]={};
@@ -33,5 +53,8 @@ int main(int argc,char * argv[],char * env[]){
 		printf("pcap_loop error!");
 		return(2);
 	}
+
+	pcap_close(handle);
+
 	return(0);
 }
